eb40aab819c96c0165094d8c57b5230f71d02a4b
nicolas.a.barbosa <nicolas.a.barbosa@accenture.com>
2019-09-10 14:24:10 -0300

    adding SimulationFormComponent

---
+++ src/app/pages/finances/simulations/simulation-form/simulation-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simulation-form',
+  templateUrl: './simulation-form.component.html',
+  styleUrls: ['./simulation-form.component.css']
+})
+export class SimulationFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
