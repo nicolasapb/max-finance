53cca56c256e4611a94cf7c8d32149a7134ab947
nicolas.a.barbosa <nicolas.a.barbosa@accenture.com>
2019-09-22 16:34:26 -0300

    SignupComponent

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 const routes: Routes = [
-  { path: '', component: SigninComponent}
+  { path: '', component: SigninComponent},
+  { path: 'signup', component: SignupComponent}
 ];
 
 @NgModule({
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 @NgModule({
-  declarations: [SigninComponent],
+  declarations: [SigninComponent, SignupComponent],
   imports: [
     SharedModule,
     HomeRoutingModule
