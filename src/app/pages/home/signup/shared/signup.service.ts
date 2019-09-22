4d4b484b1a4cd394d3477b9bf4bf9d3441dbd4b6
nicolas <nicolas.a.p.barbosa@gmail.com>
2019-09-22 18:39:50 -0300

    improving SignupComponent

--- src/app/pages/home/signup/shared/signup.service.ts
+++ src/app/pages/home/signup/shared/signup.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { NewUser } from './new-user';
+import { Observable } from 'rxjs';
 
 const API_URL = 'http://localhost:3000';
 
@@ -8,9 +10,18 @@ const API_URL = 'http://localhost:3000';
 })
 export class SignupService {
 
+  protected httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
   constructor(private http: HttpClient) { }
 
   checkUserNameTaken(userName: string) {
     return this.http.get(API_URL + '/user/exists/' + userName);
   }
+
+  signup(newUser: NewUser): Observable<NewUser> {
+    return this.http.post<NewUser>(API_URL + '/user/signup', newUser, this.httpOptions);
+  }
+
 }
--- src/app/pages/home/signup/signup.component.ts
+++ src/app/pages/home/signup/signup.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
 import { UserNotTakenValidatorService } from './shared/user-not-taken.validator.service';
+import { NewUser } from './shared/new-user';
+import { SignupService } from './shared/signup.service';
+import { ToastService } from 'src/app/core/components/toast/toast.service';
 
 @Component({
   selector: 'app-signup',
@@ -12,11 +17,14 @@ export class SignupComponent implements OnInit {
 
   public signupForm: FormGroup;
   constructor(
-    private formBuilder: FormBuilder, 
-    private userNotTakenValidatorService: UserNotTakenValidatorService
+    private formBuilder: FormBuilder,
+    private userNotTakenValidatorService: UserNotTakenValidatorService,
+    private signupService: SignupService,
+    private toastService: ToastService,
+    private router: Router
   ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.signupForm = this.formBuilder.group({
       email: [null, [Validators.required, Validators.email]],
       fullName: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(40)]],
@@ -32,6 +40,18 @@ export class SignupComponent implements OnInit {
       ],
       password: [null, [Validators.required, Validators.minLength(8), Validators.maxLength(14)]],
     });
+  }
+
+  signup(): void {
+    const newUser = this.signupForm.getRawValue() as NewUser;
+    this.signupService.signup(newUser)
+      .subscribe({
+        next: () => this.router.navigate(['']),
+        error: err => {
+          console.error(err);
+          this.toastService.show({ text: 'Ocorreu um erro no servidor', type: 'danger'});
+        }
+      });
   }
 
 }
