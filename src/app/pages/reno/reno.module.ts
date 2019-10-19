2778b0e9f7aa11f7d9a770ff787348ac81631cb4
nicolas <nicolas.a.p.barbosa@gmail.com>
2019-10-19 10:58:54 -0300

    adding RenoModule

---
+++ src/app/pages/reno/reno.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RenoRoutingModule } from './reno-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RenoRoutingModule
+  ]
+})
+export class RenoModule { }
