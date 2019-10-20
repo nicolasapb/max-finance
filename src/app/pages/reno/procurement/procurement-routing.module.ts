912334abdd5609e56e3e9a2008e5f486c70e728b
nicolas <nicolas.a.p.barbosa@gmail.com>
2019-10-20 18:04:04 -0300

    adding ProcurementFormComponent

--- src/app/pages/reno/procurement/procurement-routing.module.ts
+++ src/app/pages/reno/procurement/procurement-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProcurementListComponent } from './procurement-list/procurement-list.component';
+import { ProcurementFormComponent } from './procurement-form/procurement-form.component';
 
 
 const routes: Routes = [
@@ -8,6 +9,18 @@ const routes: Routes = [
     path: 'procurement',
     component: ProcurementListComponent
   },
+  {
+    path: 'procurement/new',
+    component: ProcurementFormComponent
+  },
+  {
+    path: 'procurement/:id/edit',
+    component: ProcurementFormComponent
+  },
+  {
+    path: 'procurement/:id/show',
+    component: ProcurementFormComponent
+  }
 ];
 
 @NgModule({
--- src/app/pages/reno/procurement/procurement.module.ts
+++ src/app/pages/reno/procurement/procurement.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { ProcurementRoutingModule } from './procurement-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ProcurementListComponent } from './procurement-list/procurement-list.component';
+import { ProcurementFormComponent } from './procurement-form/procurement-form.component';
 
 
 @NgModule({
-  declarations: [ProcurementListComponent],
+  declarations: [ProcurementListComponent, ProcurementFormComponent],
   imports: [
     SharedModule,
     ProcurementRoutingModule
